1e39c24e272f0338f43c163e7084bca19e282056
meltedelement <meltedelement@gmail.com>
2025-05-03 12:51:15 +0100

    Player has some functionality starting on a nightaction class

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,44 @@
 public class Player {
+    public String name;
+    public Role role;
+    private boolean alive;
+    private boolean isHexed;
+    private boolean isDoused;
+    private boolean isInfected;
+    private boolean attacked;
+
+    public Player(String name) {
+        this.name = name;
+        this.alive = true;
+        this.isHexed = false;
+        this.isDoused = false;
+        this.isInfected = false;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
     
+    public Role getRole() {
+        return role;
+    }
+
+
+    public void attack(){
+        attacked = true;
+    }
+
+    public void kill(){
+        alive = false;
+    }
+
+
+
+
+
 }
