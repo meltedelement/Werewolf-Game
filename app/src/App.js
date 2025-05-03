89b04a1187587eb45f74b715c1ddfa53971d7b3e
fitzylala <skye.fitzpatrick100@gmail.com>
2025-05-03 15:24:39 +0200

    Added April's app code to same repository :)

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>here is a button :0</h1>
+      <button>The Button</button>
+    </div>
+  );
+}
+
+export default App;
