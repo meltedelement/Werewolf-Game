715e04b2b7974a77b6f52ac30fb57d1a95e42af0
Yasmin Woodlock <yasminwoodlock@gmail.com>
2025-05-03 17:37:53 +0200

    random role generation good

--- src/Game.java
+++ src/Game.java
@@ -5,8 +5,10 @@ public class Game {
     public static final Roles[][] TOWN_ROLES = {RoleList.TOWN_INVESTIGATIVE_ROLES, RoleList.TOWN_NEGATIVE_ROLES,
             RoleList.TOWN_KILLING_ROLES, RoleList.TOWN_PROTECTIVE_ROLES};
 
-    private ArrayList<String> players;
+    private final ArrayList<Player> players;
+    ArrayList<Roles> gameRoles;
     private boolean useApocalypse;
+    private int villagerAmount;
     private int townRatio;
     private int neutralRatio;
     private int werewolfRatio;
@@ -15,6 +17,7 @@ public class Game {
 
     public Game(boolean useApocalypse){
         this.useApocalypse = useApocalypse;
+        this.gameRoles = new ArrayList<>();
 
         if(useApocalypse){
             this.neutralRoles = new Roles[][]{RoleList.NEUTRAL_APOCALYPSE_ROLES, RoleList.NEUTRAL_BENIGN_ROLES};
@@ -22,15 +25,30 @@ public class Game {
             this.neutralRoles = new Roles[][]{RoleList.NEUTRAL_BENIGN_ROLES};
         }
         this.players = new ArrayList<>();
-
     }
 
-    private void roleRatio(int playerAmount){
+    private void standardRoleRatio(int playerAmount){
         //1 werewolf for every 5 people
         //1 neutral for every 8 people
         werewolfRatio = (int) Math.ceil((double) playerAmount / 5);
         neutralRatio = (int) Math.ceil((double) playerAmount / 8);
-        townRatio = playerAmount - werewolfRatio - neutralRatio;
+        villagerAmount = (int) Math.ceil((double) playerAmount / 6);
+        townRatio = playerAmount - werewolfRatio - neutralRatio - villagerAmount;
+    }
+
+    public void makeRandomRoles(){
+        standardRoleRatio(players.size());
+
+        gameRoles.addAll(generateRandomWerewolf(werewolfRatio));
+        gameRoles.addAll(generateRandomNeutral(neutralRatio));
+        gameRoles.addAll(generateRandomTown(townRatio));
+        for(int i=0; i<villagerAmount; i++){
+            gameRoles.add(Roles.Villager);
+        }
+    }
+
+    public void addPlayer(String name){
+        players.add(new Player(name));
     }
 
     public void setUseApocalypse(boolean useApocalypse) {
@@ -91,5 +109,17 @@ public class Game {
 
     public ArrayList<Roles> getWerewolfRoles(int amount){
         return generateRandomWerewolf(amount);
+    }
+
+    public boolean isUseApocalypse() {
+        return useApocalypse;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public ArrayList<Roles> getGameRoles() {
+        return gameRoles;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,20 @@ public class Main {
     public static void main(String[] args) {
         Game test = new Game(true);
 
-        System.out.println(test.getTownRoles(5));
-        System.out.println(test.getNeutralRoles(2));
-        System.out.println(test.getWerewolfRoles(3));
+        test.addPlayer("Yasmin");
+        test.addPlayer("Jame");
+        test.addPlayer("April");
+        test.addPlayer("Skye");
+        test.addPlayer("Ciaran");
+        test.addPlayer("Anna");
+        test.addPlayer("Holly");
+        test.addPlayer("David");
+
+        test.makeRandomRoles();
+
+        for(int i=0; i<test.getPlayers().size(); i++){
+            System.out.println(test.getPlayers().get(i) + ": " + test.getGameRoles().get(i));
+        }
+
     }
 }
