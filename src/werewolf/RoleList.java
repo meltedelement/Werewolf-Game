e272b026c4186bc91f7a7245a088c01a77a1dd3f
meltedelement <meltedelement@gmail.com>
2025-05-03 19:27:34 +0100

    Package import kinda a bit wonky still but everything else might work

---
+++ src/werewolf/RoleList.java
@@ -0,0 +1,30 @@
+public class RoleList{
+
+    public static final Roles[] TOWN_INVESTIGATIVE_ROLES = {
+        Roles.Seer, Roles.Aura_Seer, Roles.Private_Investigator, Roles.Tracker, Roles.Clockmaker
+    };
+
+    public static final Roles[] TOWN_PROTECTIVE_ROLES = {
+        Roles.Bodyguard, Roles.Trickster, Roles.Escort, Roles.Witch
+    };
+
+    public static final Roles[] TOWN_NEGATIVE_ROLES = {
+        Roles.Cupid, Roles.Lycan, Roles.Cursed,
+    };
+
+    public static final Roles[] TOWN_KILLING_ROLES = {
+        Roles.Deputy, Roles.Hunter, Roles.Vigilante, Roles.Veteran
+    };
+
+    public static final Roles[] WEREWOLF_ROLES = {
+        Roles.Werewolf, Roles.Cubwolf, Roles.Mistwolf, Roles.Hexwolf, Roles.Sorceror, Roles.Consort
+    };
+
+    public static final Roles[] NEUTRAL_BENIGN_ROLES = {
+        Roles.Doppelganger, Roles.Tanner, Roles.Executioner
+    };
+
+    public static final Roles[] NEUTRAL_APOCALYPSE_ROLES = {
+        Roles.Arsonist, Roles.Serial_Killer, Roles.Pestilence, Roles.Grave_Digger, Roles.Doom_sayer
+    };
+}
---
+++ src/werewolf/Roles.java
@@ -0,0 +1,51 @@
+public enum Roles {
+    Villager,
+
+    //Town Investigative
+    Seer,
+    Apprentice_Seer,
+    Aura_Seer,
+    Ghost,
+    Private_Investigator,
+    Clockmaker,
+    Tracker,
+    Lookout,
+    Empath,
+
+    //Town Protective
+    Bodyguard,
+    Witch,
+    Escort,
+    Trickster,
+
+    //Town Killing
+    Deputy,
+    Vigilante,
+    Veteran,
+    Hunter,
+
+    //Town Negative
+    Cupid,
+    Lycan,
+    Cursed,
+
+    //Werewolf
+    Werewolf,
+    Sorceror,
+    Cubwolf,
+    Hexwolf,
+    Mistwolf,
+    Consort,
+
+    //Neutral benign
+    Doppelganger,
+    Tanner,
+    Executioner,
+
+    //Neutral apocalypse
+    Arsonist,
+    Serial_Killer,
+    Pestilence,
+    Grave_Digger,
+    Doom_sayer
+}
