e1a90aff6a03b0f9082a60d4537d6664c5b2b4de
Yasmin Woodlock <yasminwoodlock@gmail.com>
2025-05-04 02:43:16 +0200

    start nightActions

--- src/main/java/NightActions.java
+++ src/main/java/NightActions.java
@@ -1,16 +1,14 @@
+import java.util.Arrays;
+
 public class NightActions {
-    public void performNightAction(Roles role) {
+    public void performNightAction(Roles role, Player[] players) {
 
         switch (role) {
             case Villager:
                 break;
 
             case Seer:
-                // Action for Seer
-                break;
-
-            case Apprentice_Seer:
-                // Action for Apprentice Seer
+                seerAction(players[0]);
                 break;
 
             case Aura_Seer:
@@ -42,7 +40,7 @@ public class NightActions {
                 break;
 
             case Bodyguard:
-                // Action for Bodyguard
+                bodyGuardAction(players[0]);
                 break;
 
             case Witch:
@@ -144,5 +142,23 @@ public class NightActions {
             default:
                 throw new IllegalArgumentException("Unknown role: " + role);
         }
+    }
+
+    private void seerAction(Player targetPlayer) {
+        Roles role = targetPlayer.getrole();
+
+        if(Arrays.asList(RoleList.TOWN_PROTECTIVE_ROLES).contains(role) ||
+                Arrays.asList(RoleList.TOWN_NEGATIVE_ROLES).contains(role) ||
+                Arrays.asList(RoleList.TOWN_KILLING_ROLES).contains(role) ||
+                Arrays.asList(RoleList.TOWN_INVESTIGATIVE_ROLES).contains(role)){
+            System.out.println("thumbs up");
+        }
+        else{
+            System.out.println("thumbs down");
+        }
+    }
+
+    private void bodyGuardAction(Player targetPlayer){
+        targetPlayer.setProtected(true);
     }
 }
